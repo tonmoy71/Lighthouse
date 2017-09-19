cd8fb17fb52f64c2110877d98ebf036d531b173f
Fahim Masud Choudhury <fahim@gobd.co>
2017-09-20 00:30:07 +0600

    Setup project structure and libraries
    Project has been structured to use MVP architecture.
    Different third party libraries are added.

---
+++ app/src/main/java/io/github/tonmoy71/lighthouse/LighthouseApp.java
@@ -0,0 +1,10 @@
+package io.github.tonmoy71.lighthouse;
+
+import android.app.Application;
+
+/**
+ * Created by tonmoy on 19-Sep-17.
+ */
+
+public class LighthouseApp extends Application {
+}
---
+++ app/src/main/java/io/github/tonmoy71/lighthouse/ui/search/SearchActivity.java
@@ -0,0 +1,13 @@
+package io.github.tonmoy71.lighthouse.ui.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import io.github.tonmoy71.lighthouse.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_search);
+  }
+}
